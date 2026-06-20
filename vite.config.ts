f4ed5959efa49ed3895a7e407be8ea0f04d01005
aminmod-bit <aminmod555@gmail.com>
2026-06-20 11:50:54 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    target: "es2020",
+    cssCodeSplit: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          i18n: ["i18next", "react-i18next", "i18next-browser-languagedetector"],
+          ui: ["lucide-react", "zustand", "react-hot-toast"],
+        },
+      },
+    },
+  },
+});
